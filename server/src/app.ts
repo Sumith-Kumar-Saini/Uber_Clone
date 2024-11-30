3f0cb566e23f27622be64f52f2c25f405d6471d4
Sumith Kumar Saini <sumithsaini01@gmail.com>
2024-11-30 09:11:45 +0530

    chore: add helmet for more security

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,11 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import morgan from "morgan";
-import { errorHandler } from './middleware/error.middleware';
-import IndexRoute from './routes/index.route';
-import UserRoute from './routes/user.route';
+import { errorHandler } from "./middleware/error.middleware";
+import IndexRoute from "./routes/index.routes";
+import UserRoute from "./routes/user.routes";
 import Database from "./services/database.service";
 
 // Initialize environment variables
@@ -22,13 +23,14 @@ database.connect();
 
 // Middleware setup
 app.use(cors());
+app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/', IndexRoute);
-app.use('/user', UserRoute);
+app.use("/", IndexRoute);
+app.use("/user", UserRoute);
 
 // Error handling
 app.use(errorHandler);
